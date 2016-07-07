bbacb77cf95be25000cfda5d06db72399e87933f
Manuel <naruto06@mail.ru>
2016-07-07 03:05:45 -0700

    Initial Commit

---
+++ PhotoTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoTourist
+//
+//  Created by Мануэль on 07.07.16.
+//  Copyright © 2016 AuroraInterplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
